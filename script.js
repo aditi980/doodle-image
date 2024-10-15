1d73ec5f266126f441edc136968d7fefb183641f
aditi980 <147150679+aditi980@users.noreply.github.com>
2024-10-15 14:45:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const token ="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"
+const inputTxt = document.getElementById("input")
+const image = document.getElementById("image")
+const button = document.getElementById("btn")
+
+
+async function query() {
+    image.src = "/loading.gif"
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/black-forest-labs/FLUX.1-dev",
+		{
+			headers: {Authorization: `Bearer ${token}` },
+			method: "POST",
+			body: JSON.stringify({"inputs": inputTxt.value}),
+		}
+	);
+	const result = await response.blob();
+	return result;
+}
+
+button.addEventListener('click', async function () {
+query().then((response) => {
+	const objectURL = URL.createObjectURL(response)
+    image.src = objectURL
+});
+})
